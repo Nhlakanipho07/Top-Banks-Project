d2d46b59998041f95cf6233263df316f8f688946
Nhlakanipho07 <nhlakanipho.ngubo@umuzi.org>
2025-06-10 11:40:46 +0200

    Refactored

--- etl/help_transform_top_banks.py
+++ etl/help_transform_top_banks.py
@@ -1,4 +1,5 @@
 import csv, numpy as np, pandas as pd
+from etl.log_progress import log_progress
 
 
 def get_exchange_rates(csv_path):
@@ -8,24 +9,29 @@ def get_exchange_rates(csv_path):
         reader = csv.DictReader(file)
         for row in reader:
             exchange_rates[row["Currency"]] = float(row["Rate"])
-
     return exchange_rates
 
 
 def convert_usd(top_banks_df, df_column_name, csv_path):
-    usd_column = pd.to_numeric(top_banks_df[df_column_name])
+    exchange_rates = get_exchange_rates(csv_path)
 
     for name in [
         "MC_GBP_Billion",
         "MC_EUR_Billion",
         "MC_INR_Billion",
     ]:
-        for currency, currency_conversion_unit in get_exchange_rates(csv_path).items():
+        for currency, currency_conversion_unit in exchange_rates.items():
             if currency in name:
-                top_banks_df[name] = usd_column.apply(
+                top_banks_df[name] = top_banks_df[df_column_name].apply(
                     lambda market_cap_value: (
                         np.round(market_cap_value * currency_conversion_unit, 2)
                     )
                 )
+    return top_banks_df
 
+
+def transform(top_banks_df, csv_path):
+    top_banks_df = convert_usd(top_banks_df, "Market_cap_(USD_Billion)", csv_path)
+    print(top_banks_df)
+    log_progress("Data transformation complete. Initiating Loading process")
     return top_banks_df
